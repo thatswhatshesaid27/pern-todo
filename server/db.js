c7b95c15110bdc0bae2ccb39c4fe472839edf20b
Yash-D <90383046+thatswhatshesaid27@users.noreply.github.com>
2025-08-01 19:46:13 +0530

    changed db.js

--- server/db.js
+++ server/db.js
@@ -1,12 +1,11 @@
-const Pool = require("pg").Pool;
+const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // required for Render's managed PostgreSQL
+  },
 });
 
 module.exports = pool;
